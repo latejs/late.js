484d811fd7ae3cde890da28bd9832953e820a203
laterly <2608086516@qq.com>
2023-12-23 20:51:57 +0800

    feat: 暂不检查node版本

--- packages/lately/src/index.ts
+++ packages/lately/src/index.ts
@@ -1,16 +1,17 @@
 import pkg from '../package.json';
-import checkNodeVersion from './utils/check-node-version';
+// import checkNodeVersion from './utils/check-node-version';
 import { initCli } from './cli';
 import { defineConfig, UserConfig } from './service/config';
 import { PagesConfig } from './service/config/types';
 import mergeConfig from 'merge';
 
 // 获取允许的noe版本
-const requiredVersion = pkg.engines.node;
+// const requiredVersion = pkg.engines.node;
 // 检查node版本
-checkNodeVersion(requiredVersion, '@latelyjs/cli').then(() => {
-  // 初始化脚手架
-  initCli();
-});
+initCli();
+// checkNodeVersion(requiredVersion, '@latelyjs/lately').then(() => {
+//   // 初始化脚手架
+//   initCli();
+// });
 
 export { defineConfig, UserConfig, PagesConfig, mergeConfig };
